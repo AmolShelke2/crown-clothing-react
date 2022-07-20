6ff9a6df509419f15110924c3417f680578fd44f
Amol shelke <shelkeamol897@gmail.com>
2022-07-20 23:49:17 +0530

    Added styling to our header component

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -12,11 +12,11 @@ const Header = () => (
     </Link>
 
     <div className="options">
-      <Link className="options" to="/shop">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
 
-      <Link className="options" to="/shop">
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
     </div>
