74f71eb0a02d1a09bb3b7ebcc6353440f25c0825
Amol shelke <shelkeamol897@gmail.com>
2022-07-20 23:42:48 +0530

    Created Header component and adding Linking it to the App.js

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,7 +14,6 @@ class ShopPage extends React.Component {
 
   render() {
     const { collections } = this.state;
-    console.log(collections);
     return (
       <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
