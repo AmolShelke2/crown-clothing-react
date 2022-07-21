8bbbbee23e9f3d934a2ebc37fcf16472c33ed457
Amol shelke <shelkeamol897@gmail.com>
2022-07-21 17:54:42 +0530

    Created SignIn component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-up-and-sign-in/sign-up-and-sign-in.component";
 import Header from "./components/header/header-component";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
