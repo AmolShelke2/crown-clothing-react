5ef566b96bacf0f5b286742190e66cd65b5ba3e7
Amol shelke <shelkeamol897@gmail.com>
2022-07-23 18:44:47 +0530

    Learning firebase firestore

--- src/firebase/firebase.utlis.js
+++ src/firebase/firebase.utlis.js
@@ -22,3 +22,5 @@ provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+// Learning firebase firestore
