676724f596ae4894aaaaeae911de23ca2ab03008
Amol shelke <shelkeamol897@gmail.com>
2022-07-28 20:20:39 +0530

    Write Redux Demo Code for making it simple not confusing

---
+++ src/redux.js
@@ -0,0 +1,13 @@
+// Writing Redux Demo Code
+
+const userReducer = (currentState, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...currentState,
+        currentUser: action.payload,
+      };
+    default:
+      return currentState;
+  }
+}; // => {currentUser: {...}}
