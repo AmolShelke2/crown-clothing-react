fd43ba467ec34ce6de9c3c5df2009596b957dd71
Amol shelke <shelkeamol897@gmail.com>
2022-07-29 13:27:52 +0530

    Done with doing setUp redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
+import './index.css';
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,10 @@
-// Setting Up Redux part 2
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
