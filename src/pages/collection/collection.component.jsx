df97e774b0aef088be2adb048e6c376ca69cfcbd
Amol shelke <shelkeamol897@gmail.com>
2022-08-04 22:38:30 +0530

    Styled the Collection page and Normalize Data

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,13 +4,19 @@ import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop-selector';
 
 import './collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const collectionPage = ({ collection }) => {
-  console.log(collection);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
 
   return (
     <div className="collection-page">
-      <h2>COLLECTION PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -19,4 +25,4 @@ const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
-export default connect(mapStateToProps)(collectionPage);
+export default connect(mapStateToProps)(CollectionPage);
